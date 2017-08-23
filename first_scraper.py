5107de8758eaea1be4935464f59ff8fd9c76c9dc
marcelo <marcelo.tk3@gmail.com>
2017-08-23 12:12:32 -0500

    get title

--- first_scraper.py
+++ first_scraper.py
@@ -9,7 +9,25 @@ def get_page(file_path):
     open_file.write(html_file)
     open_file.close()
 
+
+def get_title(file_path):
+    open_file = open(file_path, "r")
+
+    regex = '<div title="buyer-name">'
+    regex_end = '</div>'
+
+    for line in open_file.readlines():
+        sentence = line.strip('\n')
+
+        if regex in sentence:
+            initial_pos = sentence.find(regex) + len(regex)
+            final_pos = sentence.find(regex_end)
+
+            print(sentence[initial_pos: final_pos])
+
+
 if __name__ == "__main__":
     file_path = "tmp.html"
 
     get_page(file_path)
+    get_title(file_path)
