6113d3bf6bcdc015ed2fba8d2de1b79d2a61eae7
marcelo <marcelo.tk3@gmail.com>
2017-08-23 17:45:21 -0500

    get title + get data = mongodb

--- third_scraper.py
+++ third_scraper.py
@@ -1,19 +1,44 @@
 from bs4 import BeautifulSoup
+from pymongo import MongoClient
 import requests
+import threading
+
 
 GOOGLE_NEWS_URL = "https://news.google.com/news/?ned=us&hl=en"
+MATCH_URL = 'gizmodo.com'
+
+
+def get_beautiful_soup(url):
+    re = requests.get(url)
+    if re.status_code == 200:
+        return BeautifulSoup(re.text, 'html.parser')
+
+
+def set_robot(article, database):
+    title = article.getText()
+    href = article.get('href')
+
+    if MATCH_URL in href:
+        soup = get_beautiful_soup(href)
+
+        if soup is not None:
+            container = soup.find('div', {'class': 'post-content entry-content js_entry-content '})
+            json = {'title': title, 'href': href, 'content': container.getText()}
+            database.articles.insert(json)
+            print('Save', title)
 
 
 def scraping_site():
-    re = requests.get(GOOGLE_NEWS_URL)
-    if re.status_code == 200:
-        soup = BeautifulSoup(re.text, 'html.parser')
+    soup = get_beautiful_soup(GOOGLE_NEWS_URL)
+    if soup is not None:
+        client = MongoClient('localhost', 27017)
+        database = client.test_scraper
 
-        if soup is not None:
-            articles = soup.find_all('a', {'class': 'nuEeue'})
+        articles = soup.find_all('a', {'class': 'nuEeue'})
 
-            for article in articles:
-                print(article.getText())
+        for article in articles:
+            robot = threading.Thread(name='set_robot', target=set_robot, args=(article, database))
+            robot.start()
 
 
 if __name__ == "__main__":
